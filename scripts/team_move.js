763b3d636a67950343fecfab26d0b8e993570bad
Seungjee Lee <moondgod@gmail.com>
2022-10-12 07:59:54 +0900

    Update Motion

--- scripts/team_move.js
+++ scripts/team_move.js
@@ -1,35 +1,48 @@
-let tl = gsap.timeline();
+const Target1 = document.querySelector("#LSJ_IMG");
+const Target2 = document.querySelector("#HJU_IMG");
+const Target3 = document.querySelector("#PJH_IMG");
+const Target4 = document.querySelector("#JYB_IMG");
+const Target5 = document.querySelector("#LJW_IMG");
 
-let intervalId = setInterval(function () {     
-    tl.to(
-        ".teamIMGs__IMGbox",{
-            x:-100,
-            rotation:80
+const handleHover = (event) =>{
+    const ID = `#${event.target.id}`;
+    console.log(ID);
+    gsap.to(
+        `${ID}`,{
+            x:20,
+            duration: 0.3,
+            ease:'elastic'
         }
     )
-    tl.to(
-        ".teamIMGs__IMGbox",{
-            y:-100,
-            rotation:160
-        }
-    )
-    tl.to(
-        ".teamIMGs__IMGbox",{
+}
+const handleHover2 = (event) => {
+    const ID = `#${event.target.id}`;
+    console.log(ID);
+    gsap.to(
+        `${ID}`,{
             x:0,
-            rotation:240
+            duration: 0.3,
+            ease:'elastic'
         }
     )
-    tl.to(
-        ".teamIMGs__IMGbox",{
-            y:0,
-            rotation:360
-        }
-    )
-    tl.to(
-        ".teamIMGs__IMGbox",{
-            y:0,
-            rotation:0
-        }
-    )
+}
 
-}, 3000);
+Target1.addEventListener("mouseover", handleHover);
+
+Target2.addEventListener("mouseover", handleHover);
+
+Target3.addEventListener("mouseover", handleHover); 
+
+Target4.addEventListener("mouseover", handleHover);
+
+Target5.addEventListener("mouseover", handleHover);
+
+Target1.addEventListener("mouseleave", handleHover2);
+
+Target2.addEventListener("mouseleave", handleHover2);
+
+Target3.addEventListener("mouseleave", handleHover2); 
+
+Target4.addEventListener("mouseleave", handleHover2);
+
+Target5.addEventListener("mouseleave", handleHover2);
