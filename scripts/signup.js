7679bb6a850499b3c42016597e5d9882eb4fc453
이승제 <106712562+Lee-Seungje@users.noreply.github.com>
2022-10-13 16:55:52 +0900

    Revert "Update User"

--- scripts/signup.js
+++ scripts/signup.js
@@ -3,40 +3,19 @@ const userBtn = document.querySelector('button');
 const userPw = document.querySelector('.pw');
 const userName = document.querySelector('.name');
 const userSchool = document.querySelector('.school');
-const check = /^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{8,16}$/;
+let regPass = /^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{8,25}$/;
+
 function onClick() {
     if (userId.value.length > 0 && userPw.value.length > 0 && userName.value.length > 0 && userSchool.value.length) {
-        userBtn.style.backgroundColor = '#c5e9ff';
+        userBtn.style.backgroundColor = '#3baff7';
         userBtn.style.color = 'black';
         userBtn.disabled = false;
     } else {
     }
 }
 
-function onBtnClick() {
-    const result = checkPassword();
-    if (result == true) {
-        userBtn.disabled = false;;
-    } else {
-        userBtn.disabled = true;
-    }
-}
-//비밀번호는 문자, 숫자, 특수문자의 조합으로 입력해주세요
-
-function checkPassword() {
-    if (!check.test(userPw.value)) {
-        alert("비밀번호는 문자, 숫자, 특수문자의 조합으로 입력해주세요");
-        return false;
-    }
-    if (userPw.length < 8 || userPw.length > 16) {
-        alert("비밀번호는 8 ~ 16 자리로 입력해주세요.");
-        return false;
-    }
-    return true;
-}
-
 userId.addEventListener('keyup', onClick);
 userPw.addEventListener('keyup', onClick);
 userName.addEventListener('keyup', onClick);
 userSchool.addEventListener('keyup', onClick);
-userBtn.addEventListener('click', onBtnClick);
+userBtn.addEventListener('click', onClick);
