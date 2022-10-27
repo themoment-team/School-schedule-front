f58dfa21dffdd6fdf4c6996a3cf1395c7880ff73
handsomejeongwoo <comand123@naver.com>
2022-10-28 08:40:37 +0900

    Update Schedule

--- scripts/clock.js
+++ scripts/clock.js
@@ -1,11 +1,9 @@
 const clock = document.querySelector("h2#clock"); //html에서 id가 clock인 h2를 가져온다
-function getClock()
-{
-    const date = new Date(); //날짜를 받아온다
-    const hours = String(date.getHours()).padStart(2, "0"); //시간을 받아온다
-    const minutes = String(date.getMinutes()).padStart(2, "0"); //분을 받아온다
-    const seconds = String(date.getSeconds()).padStart(2, "0"); //초를 받아온다
-    clock.innerText = `${hours}:${minutes}:${seconds}` //종합하여 출력한다
+function getClock() {
+  const date = new Date(); //날짜를 받아온다
+  const hours = String(date.getHours()).padStart(2, "0"); //시간을 받아온다
+  const minutes = String(date.getMinutes()).padStart(2, "0"); //분을 받아온다
+  clock.innerText = `${hours % 12}시${minutes}분`; //종합하여 출력한다
 }
 getClock();
 setInterval(getClock, 1000); //1초마다 getClock을 실행시킨다
--- scripts/schoolschedule.js
+++ scripts/schoolschedule.js
@@ -3,14 +3,17 @@ let year = today.getFullYear(); // 년도
 let month = today.getMonth() + 1; // 월
 let date = today.getDate(); // 날짜
 let YMD = `${year}${month}${date}`;
-
+const size = 2;
+// 20221023
+const YMDD = document.getElementById("YMD");
 const plus = document.getElementById("plus");
 const minus = document.getElementById("minus");
-const threeOne = [1, 3, 5, 7, 8, 10, 12];
-
-const handleClicl = () => {
-  console.log("hello");
-  console.log(YMD);
+const handleClicl = (e) => {
+  date = `${date}`;
+  date = date.padStart(2, "0");
+  YMD = `${year}${month}${date}`;
+  YMDD.innerText = YMD;
+  //console.log(YMD);
   fetch(
     `https://open.neis.go.kr/hub/hisTimetable?KEY=a810dd9ec8c04e57b5ecc4d4ff4e400e&Type=json&pIndex=1&pSize=10&ATPT_OFCDC_SC_CODE=F10&SD_SCHUL_CODE=7380292&AY=2022&SEM=2&ALL_TI_YMD=${YMD}&GRADE=1&CLASS_NM=4`
   )
@@ -28,6 +31,111 @@ const handleClicl = () => {
       }
     });
 };
+function YMDPlus() {
+  if (
+    // 31일
+    month == 1 ||
+    month == 8 ||
+    month == 3 ||
+    month == 5 ||
+    month == 7 ||
+    month == 10 ||
+    month == 12
+  ) {
+    if (date == 31) {
+      if (month == 12) {
+        month = 0;
+        year++;
+      }
+      month++;
+      date = 1;
+      //console.log(month);
+      //console.log(YMD);
+      handleClicl(1);
+      return;
+    } else {
+      date++;
+      //console.log(YMD);
+      handleClicl(5);
+    }
+  } else if (month == 2) {
+    handleClicl(2);
+    date++;
+    if (date == 28) {
+      month++;
+      //console.log(month);
+      date = 1;
+    }
+  } else if (month == 4 || month == 6 || month == 9 || month == 11) {
+    date++;
+    handleClicl(3);
+    if (date == 30) {
+      month++;
+      //console.log(month);
+      date = 1;
+      handleClicl(4);
+      return;
+    }
+  }
+}
+
+function YMDMinus() {
+  if (
+    month == 1 ||
+    month == 2 ||
+    month == 4 ||
+    month == 6 ||
+    month == 8 ||
+    month == 9 ||
+    month == 11
+  ) {
+    date--;
+    if (date == 0) {
+      month--;
+      date = 31;
+      //console.log(month);
+
+      handleClicl();
+
+      return;
+    } else {
+      handleClicl();
+
+      return;
+    }
+  } else if (month == 3) {
+    date--;
+    if (date == 0) {
+      date = 28;
+      month--;
+
+      handleClicl();
+
+      return;
+    } else {
+      handleClicl();
+
+      return;
+    }
+  } else if (month == 5 || month == 7 || month == 10 || month == 12) {
+    date--;
+    if (date == 0) {
+      month--;
+      date = 30;
+      //console.log(month);
+
+      handleClicl();
+
+      return;
+    } else {
+      handleClicl();
+
+      return;
+    }
+  }
+}
+handleClicl();
+
 /*const handleChange = (event) => {
   if (event.target == GRAInput) {
     GRADE = event.target.value;
@@ -44,77 +152,5 @@ const handleClicl = () => {
   handleClick();
 };*/
 
-function YMDPlus() {
-  switch (month) {
-    case 2: {
-      if (date >= 28) {
-        ++month;
-        date = "01";
-        YMD = `${year}${month}${date}`;
-      } else {
-        ++date;
-        YMD = `${year}${month}${date}`;
-      }
-      handleClicl();
-      break;
-    }
-    case 1:
-    case 3:
-    case 5:
-    case 7:
-    case 8:
-    case 10:
-    case 12:
-      if (date === 31) {
-        if (month == 12) {
-          month = 1;
-          date = "01";
-        } else {
-          month++;
-          date = "01";
-        }
-      } else {
-        if (date >= 10) {
-          date++;
-        } else {
-          date = `0${++date}`;
-          if (date === "010") {
-            date = "10";
-          }
-        }
-      }
-      YMD = `${year}${month}${date}`;
-      handleClicl();
-
-      break;
-    case 4:
-    case 6:
-    case 9:
-    case 11:
-      if (date === 30) {
-        month++;
-        date = "01";
-      } else {
-        if (date >= 10) {
-          date++;
-        } else {
-          date = `0${++date}`;
-          if (date === "010") {
-            console.log("sex");
-            date = "10";
-          }
-        }
-      }
-      YMD = `${year}${month}${date}`;
-      handleClicl();
-      break;
-  }
-}
-
-function YMDMinus() {
-  handleClicl();
-}
-
-handleClicl();
 plus.addEventListener("click", YMDPlus);
 minus.addEventListener("click", YMDMinus);
