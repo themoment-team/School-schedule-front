059b2a95b1b8a040e1daddd4f75b060359e3f6c4
yebin0310 <yebin060310@gmail.com>
2022-11-02 21:15:28 +0900

    Update login

--- scripts/login.js
+++ scripts/login.js
@@ -3,8 +3,9 @@ const userBtn = document.querySelector('button');
 const userPw = document.querySelector('.pw');
 const userName = document.querySelector('.name');
 const userSchool = document.querySelector('.school');
-
 const check = /^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{8,16}$/;
+let isSameID;
+let isSignUp = false;
 
 userBtn.style.backgroundColor = "white";
 function checkPassword() {
@@ -23,12 +24,12 @@ function checkPassword() {
 
 function onClick() {
     if (userId.value.length > 0 && userPw.value.length > 0) {
-        if(checkPassword() === false){
+        if (checkPassword() === false) {
             alert("비밀번호가 정규식에 맞지 않습니다");
             userBtn.disabled = true;
             window.location.reload();
             return;
-        }else{
+        } else {
             sendID = userId.value.substring;
             localStorage.setItem("userID", sendID);
             userBtn.style.backgroundColor = '#c5e9ff';
@@ -44,3 +45,59 @@ function onClick() {
 // userId.addEventListener('keyup', onClick);
 // userPw.addEventListener('keyup', onClick);
 userBtn.addEventListener('click', onClick);
+userBtn.addEventListener('click', onSubmitButton);
+
+const checkSameThing = (e) => {
+    console.log(e.target.classList[1]);
+    if (e.target.classList[1] == 'id') {
+        const idEventValue = e.target.value;
+        apiGet(idEventValue, 'id');
+    }
+    else if (e.target.classList[1] == 'pw') {
+        const pwEventValue = e.target.value;
+        return;
+    }
+}
+function apiGet(eventValue, what) {
+    let resID;
+    const uid = `${eventValue}`
+    const puts = [uid];
+    let url = 'https://server.the-moment-schema.site/overlap';
+    fetch(url, {
+        method: 'post',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            userid: uid,
+        }),
+    }
+    )
+        .then(res => {
+            return res.json();
+        })
+        .then(json => {
+            const Response = json;
+            resID = Response.data;
+            isSameID = resID;
+            console.log(isSameID);
+            const useID = document.querySelector(".useID");
+            if (isSameID == true) {
+                isSignUp = true;
+                useID.innerText = "사용 가능한 Id입니다";
+                useID.style.color = "blue";
+                useID.style.margin = 0;
+            }
+            else {
+                isSignUp = false;
+                useID.innerText = "다른 사용자가 있는 Id입니다";
+                useID.style.color = "red";
+                useID.style.margin = 0;
+                isSignUp = false;
+            }
+        })
+        .catch(err => {
+            console.log(err);
+        })
+}
+userBtn.addEventListener('click', signInBtn);
